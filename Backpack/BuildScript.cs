c1d8a3100c0b837fc5ee45031fc2e469b5b2d291
Daniel Breuer <daniel.breuer98@gmail.com>
2015-04-20 08:20:36 +0200

    finally BuildScripts

---
+++ Backpack/BuildScript.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Backpack
+{
+    class BuildScript
+    {
+        private string[] Commands;
+        private string Name;
+
+        public string Build(string path)
+        {
+            return "";
+        }
+
+        public static BuildScript LoadScriptFromFile(string path)
+        {
+            string json = File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<BuildScript>(json);
+        }
+    }
+}
