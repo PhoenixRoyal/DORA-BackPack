4db85e5a87fe559480b1523f5fb58b69608288c8
Daniel Breuer <daniel.breuer98@gmail.com>
2015-05-10 15:59:19 +0200

    shit

--- Backpack/BuildScripts.cs
+++ Backpack/BuildScripts.cs
@@ -10,13 +10,24 @@ namespace Backpack
 {
     class BuildScripts
     {
-        private string[] Commands;
-        private string Name;
+        public string[] Commands;
+        public string Name;
+        public bool Vanish;
 
         public string Build(string path)
         {
-            Process.Start("cmd.exe", "echo hi");
-            return "";
+
+            ProcessStartInfo proc = new ProcessStartInfo();
+            proc.FileName = @"C:\windows\system32\cmd.exe";
+            proc.Arguments = "/c ";
+            for (int i = 0; i < Commands.Length; i++)
+            {
+                proc.Arguments += Commands[0];
+                if(!(i == Commands.Length - 1)) proc.Arguments += " & ";
+            }
+            if (!Vanish) proc.Arguments += " & pause";
+            Process.Start(proc);
+            return proc.Arguments;
         }
 
         public static BuildScripts LoadScriptFromFile(string path)
